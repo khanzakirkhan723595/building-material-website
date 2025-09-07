37f9781ce590bee5f4cee7b600481606ecf060a3
Zakir Ahmad Khan <khanzakirkhan723595@gmail.com>
2025-09-07 19:30:53 +0530

    Day 1: Setup project with React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">
+        Building Material Website 🚧
+      </h1>
+    </div>
+  )
+}
+
+export default App
+
